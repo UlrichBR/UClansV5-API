c11ca0acdd6959f5ccb20f26bf5b2101544d210b
UlrichBR <38494475+UlrichBR@users.noreply.github.com>
2022-04-24 11:21:24 -0300

    Update ClansCommand.java

--- src/main/java/me/ulrich/clans/interfaces/ClansCommand.java
+++ src/main/java/me/ulrich/clans/interfaces/ClansCommand.java
@@ -1,5 +1,13 @@
 package me.ulrich.clans.interfaces;
 
-public interface ClansCommand {
+import java.util.EnumMap;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+import me.ulrich.clans.Clans;
+import me.ulrich.clans.data.Flag;
+
+public interface ClansCommand {
+	boolean execute(final Clans plugin, final CommandSender sender, final Command command, final String label, String[] args, EnumMap<Flag, String> info);
 }
