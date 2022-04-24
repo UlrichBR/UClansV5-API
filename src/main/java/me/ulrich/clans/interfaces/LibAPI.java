6fb138ddec682bf41184e89d552201cf2f3de9fe
UlrichBR <38494475+UlrichBR@users.noreply.github.com>
2022-04-24 19:35:04 -0300

    Add files via upload

--- src/main/java/me/ulrich/clans/interfaces/LibAPI.java
+++ src/main/java/me/ulrich/clans/interfaces/LibAPI.java
@@ -1,6 +1,7 @@
 package me.ulrich.clans.interfaces;
 
 import org.bukkit.Location;
+import org.bukkit.inventory.ItemStack;
 
 import me.ulrich.clans.library.hologram.HoloData;
 import me.ulrich.clans.library.hologram.HoloManager;
@@ -25,5 +26,15 @@ public interface LibAPI {
 	boolean deleteHolo(String id);
 
 	HoloData getHolo(String id);
+
+	ItemStack removeNbtString(ItemStack itemStack, String key);
+
+	ItemStack setNbt(ItemStack itemStack, String key, Object value);
+
+	boolean hasNbt(ItemStack currentItem, String name, String nbts);
+
+	String getNbtString(ItemStack currentItem, String name);
+
+	int getNbtInteger(ItemStack currentItem, String name);
 
 }
