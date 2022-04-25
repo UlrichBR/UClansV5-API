3d9a3f5d14959d4077225a1fa29177d3a936f448
UlrichBR <38494475+UlrichBR@users.noreply.github.com>
2022-04-25 12:15:46 -0300

    Update HoloManager.java

--- src/main/java/me/ulrich/clans/library/hologram/HoloManager.java
+++ src/main/java/me/ulrich/clans/library/hologram/HoloManager.java
@@ -1,5 +1,13 @@
 package me.ulrich.clans.library.hologram;
 
+import java.util.Collection;
+import java.util.concurrent.CopyOnWriteArraySet;
+
 public class HoloManager {
 
+	private  Collection<HoloData> holograms = new CopyOnWriteArraySet<>();
+	
+	public Collection<HoloData> getHolograms() {
+		return holograms;
+	}
 }
