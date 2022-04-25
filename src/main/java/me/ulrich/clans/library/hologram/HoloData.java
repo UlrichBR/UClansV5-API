f5bbac6213c170a98c88fac7aa4fda732a855ee8
UlrichBR <38494475+UlrichBR@users.noreply.github.com>
2022-04-25 12:15:32 -0300

    Update HoloData.java

--- src/main/java/me/ulrich/clans/library/hologram/HoloData.java
+++ src/main/java/me/ulrich/clans/library/hologram/HoloData.java
@@ -1,5 +1,22 @@
 package me.ulrich.clans.library.hologram;
 
+import org.bukkit.Location;
+
 public class HoloData {
 
+	private final Location location = null;
+	private final String uuid = "";
+	private final Object type = new Object();
+	
+    public Location getLocation() {
+        return location;
+    }
+
+    public String getUUID() {
+        return uuid;
+    }
+    
+    public Object getType() {
+		return type;
+	}
 }
