cfe1b23db5280db3dcd69d85b6968a25704dcc3c
UlrichBR <38494475+UlrichBR@users.noreply.github.com>
2022-07-11 10:18:29 -0300

    Create HologramManager.java

---
+++ src/main/java/me/ulrich/clans/manager/HologramManager.java
@@ -0,0 +1,19 @@
+package me.ulrich.clans.manager;
+
+import org.bukkit.Location;
+
+import me.ulrich.clans.interfaces.Hologram;
+
+public class HologramManager {
+
+	public boolean isHoloEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Hologram createHolo(Location location, String id, Object extra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
